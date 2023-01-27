3c6a63f405437015efe310e195d76b6834a08076
Fernando-7 <fer.maiajr@gmail.com>
2023-01-27 12:41:05 -0300

    Adjust: APIkey path and home content

--- my_app/Home.py
+++ my_app/Home.py
@@ -3,4 +3,9 @@ import streamlit as st
 st.markdown("# Home 🏠")
 st.sidebar.markdown("# Home 🏠")
 
-st.caption('Projeto com o objetivo de apresentar as vertentes referente ao chatGPT')
+st.caption('Projeto com o objetivo de apresentar as vertentes de algumas ferramentas referente ao chatGPT')
+st.subheader('Observação')
+st.text('Ao utilizar o projeto não utilize descrições com conotação.')
+st.text('pornografica ou com o intuito de obter conteúdo grotesco, a')
+st.text('ferramenta reconhece os elementos no texto e não permite')
+st.text('a realização da consulta.')
